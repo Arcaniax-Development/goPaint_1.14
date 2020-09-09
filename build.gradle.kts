114894f45f60e72bd0add573da05eb91921f6507
NotMyFault <mc.cache@web.de>
2020-09-09 18:07:56 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     "implementation"("com.sk89q.worldedit:worldedit-bukkit:7.1.0")
 }
 
-version = "1.14-3.0.0"
+version = "3.0.0"
 
 configure<LicenseExtension> {
     header = rootProject.file("HEADER")
