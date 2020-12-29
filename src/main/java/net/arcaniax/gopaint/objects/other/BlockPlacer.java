d747de969ea64af6e9d71326134433b5c8dbb659
Arcaniax <lesley.dom@hotmail.com>
2020-12-29 14:10:10 +0100

    attempt n2

--- src/main/java/net/arcaniax/gopaint/objects/other/BlockPlacer.java
+++ src/main/java/net/arcaniax/gopaint/objects/other/BlockPlacer.java
@@ -39,7 +39,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import org.bukkit.entity.Player;
-
+import net.arcaniax.gopaint.other.BlockPlace;
 import net.arcaniax.gopaint.Main;
 
 public class BlockPlacer {
