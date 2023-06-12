089fbaacb19de9bb5e9ec6b654f4ec75352b342f
TheMeinerLP <p.glanz@madfix.me>
2023-06-12 19:32:28 +0200

    Add brush settings

---
+++ src/main/java/dev/themeinerlp/bettergopaint/fawe/util/BrushSettings.java
@@ -0,0 +1,22 @@
+package dev.themeinerlp.bettergopaint.fawe.util;
+
+import dev.themeinerlp.bettergopaint.objects.other.BlockType;
+import org.bukkit.Axis;
+
+import java.util.List;
+
+public class BrushSettings {
+    public boolean surfaceEnabled;
+    public boolean maskEnabled;
+    public int chance;
+    public int thickness;
+    public int fractureDistance;
+    public int angleDistance;
+    public int falloffStrength;
+    public int mixingStrength;
+    public Axis axis;
+    public List<BlockType> blocks;
+
+    public double angleHeightDifference;
+
+}
