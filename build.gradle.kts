2ffc72fcb17c213029ab2a3cd7754d9b7086dd95
TheMeinerLP <p.glanz@madfix.me>
2023-06-12 22:54:41 +0200

    Add more supported versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 
 if (!File("$rootDir/.git").exists()) {
     logger.lifecycle(
-            """
+        """
     **************************************************************************************
     You need to fork and clone this repository! Don't download a .zip file.
     If you need assistance, consult the GitHub docs: https://docs.github.com/get-started/quickstart/fork-a-repo
@@ -46,18 +46,33 @@ group = "dev.themeinerlp.bettergopaint"
 
 val minecraftVersion = "1.20"
 val supportedMinecraftVersions = listOf(
-        "1.16.5",
-        "1.17",
-        "1.17.1",
-        "1.18",
-        "1.18.1",
-        "1.18.2",
-        "1.19",
-        "1.19.1",
-        "1.19.2",
-        "1.19.3",
-        "1.19.4",
-        "1.20"
+    "1.13",
+    "1.13.1",
+    "1.13.2",
+    "1.14",
+    "1.14.1",
+    "1.14.2",
+    "1.14.3",
+    "1.14.4",
+    "1.15",
+    "1.15.1",
+    "1.15.2",
+    "1.16.1",
+    "1.16.2",
+    "1.16.3",
+    "1.16.4",
+    "1.16.5",
+    "1.17",
+    "1.17.1",
+    "1.18",
+    "1.18.1",
+    "1.18.2",
+    "1.19",
+    "1.19.1",
+    "1.19.2",
+    "1.19.3",
+    "1.19.4",
+    "1.20"
 )
 
 repositories {
