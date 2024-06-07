24148387f0da67d72f527ba515826fec4ace2c1f
david <mrminecraft00@gmail.com>
2024-06-07 15:55:28 +0200

    removed versions that no longer work due to java version incompatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,17 +35,6 @@ group = "net.onelitefeather.bettergopaint"
 
 val minecraftVersion = "1.20.6"
 val supportedMinecraftVersions = listOf(
-    "1.16.5",
-    "1.17",
-    "1.17.1",
-    "1.18",
-    "1.18.1",
-    "1.18.2",
-    "1.19",
-    "1.19.1",
-    "1.19.2",
-    "1.19.3",
-    "1.19.4",
     "1.20",
     "1.20.1",
     "1.20.2",
