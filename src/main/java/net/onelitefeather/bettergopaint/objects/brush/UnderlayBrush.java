a7eaacdc829bbfc0e176c9ee958c36576d0168e5
david <mrminecraft00@gmail.com>
2024-06-09 03:01:32 +0200

    added new underlay brush

---
+++ src/main/java/net/onelitefeather/bettergopaint/objects/brush/UnderlayBrush.java
@@ -0,0 +1,64 @@
+/*
+ * goPaint is designed to simplify painting inside of Minecraft.
+ * Copyright (C) Arcaniax-Development
+ * Copyright (C) Arcaniax team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.onelitefeather.bettergopaint.objects.brush;
+
+import net.onelitefeather.bettergopaint.brush.BrushSettings;
+import net.onelitefeather.bettergopaint.utils.Sphere;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class UnderlayBrush extends Brush {
+
+    @Override
+    public void paint(final Location location, final Player player, final BrushSettings brushSettings) {
+        performEdit(player, session -> {
+            List<Block> blocks = Sphere.getBlocksInRadius(location, brushSettings.getSize());
+            for (Block block : blocks) {
+                if (!passesDefaultChecks(brushSettings, player, block)) {
+                    continue;
+                }
+
+                if (!isUnderlay(block, brushSettings.getThickness())) {
+                    continue;
+                }
+
+                setBlock(session, block, brushSettings.getRandomBlock());
+            }
+        });
+    }
+
+    private boolean isUnderlay(Block block, int thickness) {
+        for (int i = 1; i <= thickness; i++) {
+            if (block.getRelative(BlockFace.UP, i).isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "Underlay Brush";
+    }
+
+}
